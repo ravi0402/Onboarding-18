e13f8f026ca8a835f81a9658659dfb4350a1a132
Ravi Sharnagat <ravi.sharnagat@gmail.com>
2018-06-22 14:11:01 +0530

    components and composition

---
+++ lists.py
@@ -0,0 +1,46 @@
+
+
+def array_test():
+	arr = [1,2,3,4,5]
+	arr.reverse()
+	print("%s")%arr.index(4)
+	print(arr)
+
+
+def main():
+	array_test()
+
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
